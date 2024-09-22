87645e17df811b19fcc9af6b531abef49c659cca
alexfila <fialex66@gmail.com>
2024-09-23 00:22:16 +0200

    Initial Commit

---
+++ Animation-SwiftUI/Animation_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Animation_SwiftUIApp.swift
+//  Animation-SwiftUI
+//
+//  Created by Alex Fila on 23/9/24.
+//
+
+import SwiftUI
+
+@main
+struct Animation_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Animation-SwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Animation-SwiftUI
+//
+//  Created by Alex Fila on 23/9/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
