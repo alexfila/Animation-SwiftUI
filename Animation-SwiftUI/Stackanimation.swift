456c2d9fa76cb0f3acc84f8ab6f55b3d05f578db
Alex Fila <fialex66@gmail.com>
2024-09-30 22:03:23 +0200

    adding stack animation

---
+++ Animation-SwiftUI/Stackanimation.swift
@@ -0,0 +1,18 @@
+//
+//  Stackanimation.swift
+//  Animation-SwiftUI
+//
+//  Created by Alex Fila on 30/09/24.
+//
+
+import SwiftUI
+
+struct Stackanimation: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Stackanimation()
+}
